dd18d0208b99d66e2d5bee72eb61975296b43560
Honza Kváš <kvasik98@seznam.cz>
2022-09-01 13:28:14 +0200

    redirect after created project

--- src/views/AddProjectFormView.vue
+++ src/views/AddProjectFormView.vue
@@ -6,6 +6,7 @@
 <script>
 import { computed, onMounted, provide, reactive } from 'vue';
 import { useStore } from 'vuex'
+import { useRouter } from 'vue-router'
 //import AddProjectForm from "../components/Forms/AddProjectForm.vue";
 import Formes from "../components/Forms/Form.vue";
 import Project from "../utils/project"
@@ -19,11 +20,13 @@ export default {
 
   setup() {
     const store = useStore()
+    const router = useRouter()
     const newProject = new Project()
 
     function addNewProject(newProject){
       console.log(newProject)
       store.dispatch("addNewProject", newProject);
+      router.push('/projects')
     }
 
     return {
